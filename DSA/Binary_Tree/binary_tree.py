1e5577e9bfc77f27510d674bb7312ab9323c852e
Moin Ali Khan <moinalikhandevopser@gmail.com>
2022-12-11 12:37:57 +0530

    binary tree

--- DSA/Binary_Tree/binary_tree.py
+++ DSA/Binary_Tree/binary_tree.py
@@ -1,91 +1,94 @@
+from queueLinkedList import queueLinkedList
 
-class node:
-    __slots__ = "element","left","right"
-    def __init__(self,element,left=None,right=None):
-        self.element = element
-        self.left = left
-        self.right = right
-    
+class _Node:
+    __slots__ = '_element', '_left', '_right'
+    def __init__(self, element, left, right ):
+        self._element = element
+        self._left = left
+        self._right = right
 
-class binary_tree:
+class bibaryTree:
     def __init__(self):
-        self.root = None
+        self._root = None
+        
+    def maketree(self, e, left, right):
+        self._root = _Node(e, left._root, right._root)
 
-    def maketree(self,e,left,right):
-        self.root = node(e,left.root,right.root)
-
-    def inorder(self,troot):
+    def inorder(self, troot):
         if troot:
-            self.inorder(troot.left)
-            print(troot.element,end=" ")
-            self.inorder(troot.right)
-    
-    def preorder(self,troot):
-        if troot:
-            print(troot.element,end=" ")
-            self.preorder(troot.left)
-            self.preorder(troot.right)
+            self.inorder(troot._left)
+            print(troot._element, end = ' ')
+            self.inorder(troot._right)
 
-    def postorder(self,troot):
+    def preorder(self, troot):
         if troot:
-            self.postorder(troot.left)
-            self.postorder(troot.right)
-            print(troot.element,end=' ')
+            print(troot._element, end = ' ')
+            self.preorder(troot._left)
+            self.preorder(troot._right)
 
-    # def levelorder(self,troot):
-    #     Q = queueslinked()
-    #     t = self.root
-    #     print(t.element,end=" ")
-    #     Q.enqueue(t)
-    #     while not Q.isempty():
-    #         t = Q.dequeue()
-    #         if t.left:
-    #             print(t.left.element,end=" ")
-    #             Q.enqueue(t.left)
-    #         if t.right:
-    #             print(t.root.element,end=' ')
-    #             Q.enqueue(t.right)
-
-    def count(self,troot):
+    def postorder(self, troot):
         if troot:
-            x = self.count(troot.left)
-            y = self.count(troot.right)
-            return x+y+1
+            self.postorder(troot._left)
+            self.postorder(troot._right)
+            print(troot._element, end = ' ')
+
+    def levelorder(self, troot):
+        Q = queueLinkedList()
+        t = troot
+        print(troot._element, end = ' ')
+        Q.enqueue(t)
+        while not Q.isempty():
+            t = Q.dequeue()
+            if t._left:
+                print(t._left._element, end = ' ')
+                Q.enqueue(t._left)
+            if t._right:
+                print(t._right._element, end = ' ')
+                Q.enqueue(t._right)
+
+    def count(self, troot):
+        if troot:
+            x = self.count(troot._left)
+            y = self.count(troot._right)
+            return x + y + 1
         return 0
 
-    def height(self,troot):
+    def height(self, troot):
         if troot:
-            x = self.height(troot.left)
-            y = self.height(troot.right)
+            x = self.height(troot._left)
+            y = self.height(troot._right)
             if x > y:
                 return x + 1
             else:
                 return y + 1
         return 0
 
-x=binary_tree()
-y=binary_tree()
-z=binary_tree()
-a=binary_tree()
-# r=binary_tree()
-# s=binary_tree()
-# t=binary_tree()
-x.maketree(40,a,a)
-y.maketree(50,a,a)
-z.maketree(30,x,y)
-# r.maketree(50,a,y)
-# s.maketree(30,r,a)
-# t.maketree(10,z,s)
-print("inorder traversel")
-z.inorder(z.root)
+
+x = bibaryTree()
+y = bibaryTree()
+z = bibaryTree()
+r = bibaryTree()
+s = bibaryTree()
+t = bibaryTree()
+a = bibaryTree()
+x.maketree(40, a, a)
+y.maketree(60, a, a)
+z.maketree(20, x, a)
+r.maketree(50, a, y)
+s.maketree(30, r, a)
+t.maketree(10, z, s)
+print("Inorder Traversal")
+t.inorder(t._root)
 print()
-print("preorder traversel")
-z.preorder(z.root)
+print("Preorder Traversal")
+t.preorder(t._root)
 print()
-print("postorder traversel")
-z.postorder(z.root)
+print("Postorder Traversal")
+t.postorder(t._root)
 print()
-print("number of nodes")
-print(z.count(z.root))
-print("height")
-print(z.height(z.root)-1)
+print("Levelorder Traversal")
+t.levelorder(t._root)
+print()
+print("Number of Nodes : ", t.count(t._root))
+print()
+print("Height of the Tree : ", t.height(t._root)-1)
